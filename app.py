e9b672451a5f7a13d624751d3f761eebcfca5809
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2024-09-19 20:36:03 +0800

    complete table

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pypinyin import lazy_pinyin, Style
 
 from collections import Counter
 
-zh_punctuation = string.punctuation + '，。！？；：“”‘’（）【】《》 '
+zh_punctuation = string.punctuation + '，。！？；：“”‘’（）【】《》   \n'
 
 def remove_punctuation(text):
     return text.translate(str.maketrans('', '', zh_punctuation))
@@ -20,8 +20,8 @@ chapter = remove_punctuation(chapter)
 
 if chapter:
     # Display the entered text
-    st.write('Text:')
-    st.write(chapter)
+    # st.write('Text:')
+    # st.write(chapter)
 
     # Parse text
     words = jieba.cut(chapter)
@@ -31,27 +31,28 @@ if chapter:
     # Sort words by frequency
     word_counter = Counter(word_list)
     word_ranking = [item for items, count in word_counter.most_common() for item in [items]]
+
+    # Count occurences
     word_count = [count for items, count in word_counter.most_common()]
-    # word_ranking_unique = [word_ranking_unique.append(x) for x in word_ranking if x not in word_ranking_unique]
-
-    # Count frequency
-
+    
     # Calculate total percentage
-
-    # Remove duplicates
+    total_frequency = sum(word_count)
+    print(f'Total Frequency: {total_frequency}')
+    word_percentages = [count/total_frequency*100 for count in word_count]
 
     # Get the pinyin
-    pinyin_pairs = dict()
-    for word in word_list:
-        pinyin_pairs[word] = lazy_pinyin(word, style=Style.TONE3)[0]
+    word_pinyin = [lazy_pinyin(word, style=Style.TONE3)[0] for word in word_ranking]
 
 
     # Create a sample DataFrame
     df = pd.DataFrame(
-        columns=("Word", "Pinyin", "Frequency", "Occurences")
+        columns=("Word", "Pinyin", "%", "Occurences"),
     )
 
     df['Word'] = pd.Series(word_ranking)
     df['Occurences'] = pd.Series(word_count)
+    df['%'] = pd.Series(word_percentages)
+    df['Pinyin'] = pd.Series(word_pinyin)
+
     # Display the DataFrame as an interactive table
-    st.dataframe(df)
+    st.dataframe(df, width=550)
